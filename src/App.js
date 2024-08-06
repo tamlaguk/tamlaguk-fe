be69cd05fe5c2cb413f458d381b10da1fd08364b
collie-jun <167491677+collie-jun@users.noreply.github.com>
2024-08-06 18:51:59 +0900

    app.js 페이지 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/mainPage";
 import LoginPage from "./pages/loginPage";
 import ReviewPage from "./pages/reviewPage";
+import TourPage from "./pages/tourPage";
+import FoodPage from "./pages/foodPage";
+import LeisurePage from "./pages/leisurePage";
+
 function App() {
   return (
     <Router>
@@ -10,6 +14,9 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/main" element={<MainPage />} />
         <Route path="/review" element={<ReviewPage />} />
+        <Route path="/food" element={<FoodPage />} />
+        <Route path="/tour" element={<TourPage />} />
+        <Route path="/leisure" element={<LeisurePage />} />
       </Routes>
     </Router>
   );
